f2e4f940abc28895df9fe1175832273060707ce7
zoekeihani <zoekeihani@gmail.com>
2025-12-08 19:31:21 -0600

    second commit

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import requests
-import xml.etree
+import xml.etree.ElementTree as ET
 import re
 
 class PubMed:
     def __init__(self):
         self.api_base_url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/"
+        self.qk = ""
+        self.webenv = ""
 # request to get url
     def test_query(self):
         r = requests.get(self.api_base_url)
@@ -18,24 +20,42 @@ class PubMed:
         r = requests.get(self.api_base_url + "esearch.fcgi?db=pubmed&term="+keyword+"&usehistory=y")
         # print("esearch Result: " + r.text)
 
-        qk = ""
-        webenv = ""
-
         for line in r.text.splitlines():
             qk_match = re.search("<QueryKey>(.+?)</QueryKey>", line)
             webenv_match = re.search("<WebEnv>(.+?)</WebEnv>", line)
 
             if qk_match:
-                qk = qk_match.group(1)
+                self.qk = qk_match.group(1)
 
             if webenv_match:
-                webenv = webenv_match.group(1)
+                self.webenv = webenv_match.group(1)
 
-        print(f"query_key: {qk}\nwebenv: {webenv}")
+        print(f"query_key: {self.qk}\nwebenv: {self.webenv}")
 
-    def esummary(self, qk, webenv):
-        r = requests.get(self.api_base_url + f"esummary.fcgi?db=pubmed&query_key={qk}&WebEnv={webenv}")
-        print("esummary Result: "+ r.text)
+    def esummary(self):
+        r = requests.get(self.api_base_url + f"esummary.fcgi?db=pubmed&query_key={self.qk}&WebEnv={self.webenv}")
+        print(f'esummary: "{r.text}"')
+        return r.text
+
+
+
+
+class PubMedSearch:
+    def __init__(self, xml_text):
+        self.root = ET.fromstring(xml_text)
+        self.all_docs = self.root.findall(".//DocSum")
+    def get_titles(self):
+        titles = []
+        for docsum in self.all_docs:
+            for item in docsum.findall("Item"):
+                if item.attrib.get("Name") == "Title":
+                    titles.append(item.text)
+        return titles
+
+
+
+
+
 
 
 if __name__ == "__main__":
@@ -43,8 +63,20 @@ if __name__ == "__main__":
     # pubmed.test_query()
     # pubmed.einfo()
     # pubmed.esearch()
-    #pubmed.esearch("fbn1")
-    pubmed.esummary(1, "MCID_69336c0ee7c255073e09f504")
+    pubmed.esearch("fbn1")
+    pubmed.esummary()
+    xml_text = pubmed.esummary()
+    summary = PubMedSearch(xml_text)
+    titles = summary.get_titles()
+
+    print("Article Titles:")
+    for t in titles:
+        print("-", t)
+
+
+
+
+
 
 
 
@@ -52,4 +84,5 @@ if __name__ == "__main__":
 Esearch = list of UID 
 ESummary = feeding the UID and getting summary
 EFetch = feed the UID and get some record
+load the esummary result  create class and object and then print the name of each article 
 '''
